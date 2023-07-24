14869c7ee3b1345a497d19a9d80ae353f395e4f9
Anton Gusev <antongusev705@gmail.com>
2023-07-24 09:35:56 +0300

    Задание 52

---
+++ 3/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+public static class ArrayExample
+{
+    public static void Main()
+    {
+        int[,] array = {
+            { 13, 24, 33, 45 },
+            { 53, 62, 71, 80 },
+            { 97, 10, 11, 12 }
+        };
+
+        Console.WriteLine("Заданный массив:");
+        PrintArray(array);
+
+        int rows = array.GetLength(0);
+        int columns = array.GetLength(1);
+
+        double[] columnAverages = new double[columns];
+
+        for (int j = 0; j < columns; j++)
+        {
+            double sum = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                sum += array[i, j];
+            }
+            columnAverages[j] = sum / rows;
+        }
+
+        Console.WriteLine("Среднее арифметическое каждого столбца:");
+        PrintColumnAverages(columnAverages);
+    }
+
+    public static void PrintArray(int[,] array)
+    {
+        int rows = array.GetLength(0);
+        int columns = array.GetLength(1);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public static void PrintColumnAverages(double[] columnAverages)
+    {
+        foreach (double average in columnAverages)
+        {
+            Console.Write(average.ToString("0.0") + "; ");
+        }
+        Console.WriteLine();
+    }
+}
+
